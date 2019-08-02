278ad9aa84a361a7f342e599e4d31e4614030890
Raven Robertson <ravewillow6383@gmail.com>
2019-08-01 21:43:49 -0700

    building files

---
+++ app/api/users.py
@@ -0,0 +1,40 @@
+from flask import jsonify, request, url_for, g, abort
+from app import db
+from app.models import User
+from app.api import bp
+from app.api.auth import token_auth
+from app.api.errors import bad_request
+
+@bp route('/users/<int:id>', methods=['GET'])
+@token__auth.login_required
+def get_users():
+    data = [user.to_dict() for user User.query.all()]
+    return jsonify(data)
+
+@bp.route('/users', methods=['POST'])
+def create_user():
+    data = request.get json() or {}
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('must include username, email and password fields')
+        if User.query.filter_by(username=data['username']).first():
+            return bad_request('please use a different username.')
+        if User.query.filter_by(email=data['email']).first():
+            return bad_request('please use a different email.')
+        user = User()
+        user.from_dict(data, new_user=True)
+        db.session.add(user)
+        db.session.commit()
+        response = jsonify(user.to_dict())
+        response.status_code = 201
+        resonse.headers['Location'] = url_for('api.get_user', id=user.id)
+        return response
+
+        @bp.route('/users/<int:id>', methods=['PUT'])
+        @token_auth.login_required
+        def update_user(id):
+            if g.current_user.id != id:
+                about(403)
+            user = User.query.get_or_404(id)
+            data = request.get_json() or {}
+            if 'username' in data and data['username'] != user.username and \
+                User.query.filter_by(email=data['email']).first()
